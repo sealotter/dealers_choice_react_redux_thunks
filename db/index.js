6823cac48ff1ae99dcef12c216de1c684c3b6590
Anna Kohler <a.kohler.capa@gmail.com>
2022-03-13 22:18:26 -0400

    added data with faker

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:banana794@localhost/acme-react-redux')
-const {STRING, UUIDV4, UUID} = Sequelize
+const {STRING, UUIDV4, UUID, TEXT} = Sequelize
 const faker = require('faker')
 
 
@@ -16,8 +16,39 @@ const Brewery = conn.define('brewery', {
         validate: {
             notEmpty: true
         }
+    },
+    address: {
+        type: STRING
+       
+    },
+    about: {
+        type: TEXT
+    
     }
 })
+
+const createBrew = async(brewNum) => {
+    try{
+        let i = 0
+        while(i < brewNum) {
+    
+            await Brewery.create({
+                name: faker.company.companyName(),
+                address: faker.address.streetAddress(),
+                about: faker.lorem.sentences()
+            })
+    
+            i++
+    
+        }
+
+    }
+    catch(ex){
+        console.log(ex)
+    }
+   
+}
+
 
 Brewery.generateRandom = function() {
     return this.create({name: faker.company.companyName()})
@@ -26,11 +57,15 @@ Brewery.generateRandom = function() {
 const syncAndSeed = async() => {
     try{
         await conn.sync({force: true})
-        const brewing = await Brewery.create({name: 'Brewing Company'})
-        const barrel = await Brewery.create({name: 'Barrel Brewing Co.'})
-        const bros = await Brewery.create({name: 'Bros and Barrels'})
-        const bruster = await Brewery.create({name: 'Bruster Brewing Co.'})
-        const acres = await Brewery.create({name: '12 Acres Brewing Company'})
+        await createBrew(20)
+
+       
+
+        // const brewing = await Brewery.create({name: faker.company.companyName()})
+        // const barrel = await Brewery.create({name: 'Barrel Brewing Co.'})
+        // const bros = await Brewery.create({name: 'Bros and Barrels'})
+        // const bruster = await Brewery.create({name: 'Bruster Brewing Co.'})
+        // const acres = await Brewery.create({name: '12 Acres Brewing Company'})
         
     }catch(ex) {
         console.log(ex)
